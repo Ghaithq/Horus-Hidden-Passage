f7d49f730b2760487fe6ce7cb2feaa62a320e4e3
Amr Elsheshtawy <96791769+Amreux@users.noreply.github.com>
2024-04-16 05:27:37 +0200

    feat:pipeline-state&material_fix:transform

--- source/common/ecs/world.hpp
+++ source/common/ecs/world.hpp
@@ -26,7 +26,10 @@ namespace our {
         Entity* add() {
             //TODO: (Req 8) Create a new entity, set its world member variable to this,
             // and don't forget to insert it in the suitable container.
-            return nullptr;
+            Entity* newEntity=new Entity();
+            newEntity->world=this;
+            entities.insert(newEntity);
+            return newEntity;
         }
 
         // This returns and immutable reference to the set of all entites in the world.
@@ -38,17 +41,34 @@ namespace our {
         // The elements in the "markedForRemoval" set will be removed and deleted when "deleteMarkedEntities" is called.
         void markForRemoval(Entity* entity){
             //TODO: (Req 8) If the entity is in this world, add it to the "markedForRemoval" set.
+            auto it=entities.find(entity);
+            if(it!= entities.end()){
+                markedForRemoval.insert(entity);
+            }
         }
 
         // This removes the elements in "markedForRemoval" from the "entities" set.
         // Then each of these elements are deleted.
         void deleteMarkedEntities(){
             //TODO: (Req 8) Remove and delete all the entities that have been marked for removal
+            for(auto markedEntity=markedForRemoval.begin();markedEntity!=markedForRemoval.end();std::advance(markedEntity,1)){
+                auto iter=entities.find(*markedEntity);
+                if(iter!=entities.end()){
+                    delete *iter;
+                    entities.erase(iter);
+                }
+            }
+            markedForRemoval.clear();
         }
 
         //This deletes all entities in the world
         void clear(){
             //TODO: (Req 8) Delete all the entites and make sure that the containers are empty
+            for(auto iter=entities.begin();iter!=entities.end();std::advance(iter,1)){
+                delete *iter;
+            }
+            entities.clear();
+            markedForRemoval.clear();
         }
 
         //Since the world owns all of its entities, they should be deleted alongside it.
